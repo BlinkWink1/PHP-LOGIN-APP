2cee8513c40835d8c922df952a59a951e082ae9b
ZakariaCB <118432598+BlinkWink1@users.noreply.github.com>
2025-06-02 21:52:20 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,15 +6,15 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="container">
-  <h1>PROYECTO TFC ZAKARIA CHIOULOUD</h1>
-  <?php if (isset($_SESSION['username'])): ?>
-  <p>Hola, <b><?= $_SESSION['username'] ?></b></p>
-  <a href="logout.php"><button>Cerrar sesión</button></a>
-<?php else: ?>
-    <a href="register.php"><button>Registrarse</button></a>
-    <a href="login.php"><button>Iniciar sesión</button></a>
-  <?php endif; ?>
-</div>
+  <div class="container">
+    <h1>PROYECTO TFC ZAKARIA CHIOULOUD</h1>
+    <?php if (isset($_SESSION['username'])): ?>
+      <p>Hola, <b><?= $_SESSION['username'] ?></b></p>
+      <a href="logout.php"><button>Cerrar sesión</button></a>
+    <?php else: ?>
+      <a href="register.php"><button>Registrarse</button></a>
+      <a href="login.php"><button>Iniciar sesión</button></a>
+    <?php endif; ?>
+  </div>
 </body>
 </html>
